154b9f6f0266a7bf547579fb6022ecc7ed28d4f7
Enzo Pontet <enzopch2022@gmail.com>
2023-03-16 13:34:14 -0300

    AHORA SI GATO XDXDXD Se agrego Modal Pa Subir Publicacion u evento, se agrego comment, y se agrego DetailPlaces

---
+++ src/Componentes/ParaSubida/ModalSubida.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import RellenoModal from './RellenoModal'
+
+const ModalSubida = () => {
+  return (
+    <>
+
+<button className='btn-success mx-auto'>
+<label htmlFor="my-modal-3">
+ABREME
+</label>
+</button>
+
+
+   <input type="checkbox" id="my-modal-3" className="modal-toggle" />
+<div className="modal">
+  <div className="modal-box relative max-w-screen-md" >
+    <label htmlFor="my-modal-3"  className="btn btn-sm btn-circle absolute right-2 top-2 input-info">✕</label>
+  <RellenoModal nombre="Publicación"/>
+  </div>
+</div>
+    
+    </>
+  )
+}
+
+export default ModalSubida
---
+++ src/Componentes/ParaSubida/RellenoModal.jsx
@@ -0,0 +1,68 @@
+import React from 'react'
+
+const RellenoModal = (props) => {
+  // las props es para cuando lo llamemos aparezca evento o publicaion xd
+  return (
+    <>
+    <h1 className='mb-3'>Agregar {props.nombre}</h1>
+    
+    <div className="grid grid-cols-2 gap-5 mb-4">
+        <div className='col-span-2  sm:col-span-1'>
+
+            <label >Nombre del Lugar</label>
+            <input type="text" className='input input-bordered input-info w-full max-w-xs mt-2' />
+        </div>
+
+<div className='col-span-2  sm:col-span-1'>
+
+<label >Ubicación</label>
+<input type="text" className='input input-bordered input-info w-full  mt-2' />
+</div>
+
+
+        <div className='col-span-2  sm:col-span-2'>
+
+            <label >Descripción</label>
+            
+
+           <textarea  className='input input-bordered input-info mt-2 p-3 w-full  py-1.5 shadow-sm ring-1 ring-inset focus:ring-2 focus:ring-inset sm:text-sm sm:leading-6 ' >
+
+                      </textarea>
+
+                     
+        </div>
+
+        <div className='col-span-2  sm:col-span-2'>
+
+            
+            <input type="file" className="file-input file-input-bordered file-input-primary w-full max-w-xs" />
+        </div>
+        
+
+    </div>
+
+    <hr className='input-info'/>
+
+    <div className="grid-cols-1">
+
+    <div className='col-span-1 sm:text-end text-center'>
+
+    <button
+                    type="submit"
+                    htmlFor="my-modal-3"
+                    id="my-modal-3"
+                    className="inline-flex btn btn-info w-20 text-end mt-4 rounded-md py-2 px-3 text-sm font-semibold text-white shadow-sm  focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
+                  >
+                    <label htmlFor="my-modal-3">
+
+                    Publicar
+                    </label>
+                  </button>
+    </div>
+    </div>
+
+    </>
+  )
+}
+
+export default RellenoModal
